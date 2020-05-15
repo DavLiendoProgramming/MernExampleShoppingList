2ecaf5723e26476e70dc5c4d8f8a9696f00a2eeb
David Liendo <dliendo05@gmail.com>
2020-05-15 09:17:45 -0400

    make it better

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : `mongodb+srv://admin:1234@cluster0-cpeqo.mongodb.net/test?retryWrites=true&w=majority`
+};
---
+++ models/Item.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+//Create Squema
+
+const  ItemSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = Item = mongoose.model('item', ItemSchema);
